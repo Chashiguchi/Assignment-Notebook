6481867ff139f18fe4af9bb8469e9f29572fd7e0
Chase Hashiguchi <chashiguchi@bsd220.org>
2024-02-02 09:50:01 -0600

    Created initial Assignment Notebook

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItems =
+    [AssignmentItem(priority: "High", description: "Take out trash", dueDate: Date()),
+     AssignmentItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+     AssignmentItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(assignmentItems) {item in
+                    Text(item.description)
+                }
+                .onMove {indices, newOffset in
+                    assignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+                }
+                .onDelete { indexSet in
+                    assignmentItems.remove(atOffsets: indexSet)
+                }
+            }
+                .navigationBarTitle("Assignments", displayMode: .inline)
+                .navigationBarItems(leading: EditButton())
         }
-        .padding()
     }
 }
 
@@ -23,4 +35,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+    var priority = String()
+    var description = String()
+    var dueDate = Date()
 }
