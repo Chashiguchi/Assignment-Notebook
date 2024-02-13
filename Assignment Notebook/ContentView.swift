c12b29af531525f083c3d9606206e186e4679c86
Chase Hashiguchi <chashiguchi@bsd220.org>
2024-02-12 23:16:55 -0600

    Added Color and Borders

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -10,32 +10,45 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject var assignmentList = AssignmentList()
     @State private var showingAddAssignmentView = false
+    
     var body: some View {
-        NavigationView{
-            List{
-                ForEach(assignmentList.items) {item in
-                    HStack {
-                        VStack(alignment: .leading) {
+        NavigationView {
+            List {
+                ForEach(assignmentList.items) { item in
+                    VStack(alignment: .leading) {
+                        HStack {
                             Text(item.priority)
                                 .font(.headline)
-                            Text(item.description)
+                            Spacer()
+                            Text(item.dueDate, style: .date)
                         }
-                        Spacer()
-                        Text(item.dueDate, style: .date)
+                        .padding(.vertical, 8) // Add vertical padding so it doesnt interefere with the top of the text
+                        .padding(.horizontal, 8)// Add horizOntal padding so it doesnt interfere with the side of the text
+                        .background(Color.cyan) // Set background color
+                        .border(Color.black, width: 2) // Add border
+                        .cornerRadius(7) // Rounded corners
+                        Text(item.description)
+                            .padding(.leading, 8)
+                            .padding(.vertical, 8) // Add vertical padding
+                            .padding(.horizontal, 4)// Add horizOntal padding
+                            .background(Color.cyan) // Set background color
+                            .border(Color.black, width: 2) // Add border
+                            .cornerRadius(7) // Rounded corners
                     }
                 }
                 .onMove { indices, newOffset in
                     assignmentList.items.move(fromOffsets: indices, toOffset: newOffset)
                 }
-                .onDelete{ indexSet in
+                .onDelete { indexSet in
                     assignmentList.items.remove(atOffsets: indexSet)
                 }
             }
-            .sheet(isPresented: $showingAddAssignmentView, content: {
+            .sheet(isPresented: $showingAddAssignmentView) {
                 AddAssignmentView(assignmentList: assignmentList)
-            })
+            }
             .navigationBarTitle("Assignments", displayMode: .inline)
-            .navigationBarItems(leading: EditButton(), trailing: Button(action: { showingAddAssignmentView = true}) { Image(systemName: "plus")
+            .navigationBarItems(leading: EditButton(), trailing: Button(action: { showingAddAssignmentView = true }) {
+                Image(systemName: "plus")
             })
         }
     }
@@ -46,6 +59,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
 struct AssignmentItem: Identifiable, Codable {
     var id = UUID()
     var priority = String()
